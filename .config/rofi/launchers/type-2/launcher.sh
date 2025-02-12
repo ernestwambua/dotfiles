c6778d866c0b5860fdb7f6dc6a07caaefd9b3b63
Ernest Wambua <ernestwambua3@gmail.com>
2025-02-12 20:59:56 +0300

    Fixed rofi config

--- .config/rofi/launchers/type-2/launcher.sh
+++ .config/rofi/launchers/type-2/launcher.sh
@@ -9,11 +9,12 @@
 #
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
+## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/launchers/type-4"
-theme='style-1'
+dir="$HOME/.config/rofi/launchers/type-2"
+theme='style-8'
 
 ## Run
 rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+  -show drun \
+  -theme ${dir}/${theme}.rasi
