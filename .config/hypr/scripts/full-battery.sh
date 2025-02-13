febfa3df3f33235592ff77ad2df41ae789f0eb3e
Ernest Wambua <ernestwambua3@gmail.com>
2025-02-13 12:42:13 +0300

    Changed low battery threshold; Added full battery script

--- .config/hypr/scripts/full-battery.sh
+++ .config/hypr/scripts/full-battery.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-threshold=15
+threshold=98
 
 while true; do
   percentage=$(upower -i "$(upower -e | grep BAT)" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
   state=$(upower -i "$(upower -e | grep BAT)" | grep -E "state" | awk '{print $2}')
 
-  if [ "$percentage" -le "$threshold" ] && [ "$state" = "discharging" ]; then
-    hyprctl notify 0 5000 0 "  Low battery: ${percentage}%  " &&
-      pw-play ~/.config/hypr/scripts/low-battery.mp3
+  if [ "$percentage" -ge "$threshold" ] && [ "$state" = "charging" ]; then
+    hyprctl notify 0 5000 0 "  Full battery: ${percentage}%  " &&
+      pw-play ~/.config/hypr/scripts/full-battery.mp3
     sleep 240
   else
     sleep 120
